def0262ee4464f42b82e151a1e85c58d56d6f34d
Jiayu-Yin <159534253+Jiayu-Yin@users.noreply.github.com>
2024-05-04 16:27:33 +0100

    Add files via upload

---
+++ task_e.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(int argc, char *argv[]){
+    
+	
+	// Enter your code under here to read the filename from the command line
+	    if (argc != 2) {
+        printf("Usage: %s filename\n", argv[0]);
+        return 1; 
+    }
+	
+	
+	// Read number from file - do not edit
+	double num;
+	FILE *fptr;
+	fptr = fopen(argv[1], "r");
+	fscanf(fptr, "%lf", &num);
+	fclose(fptr);
+	
+
+	// Enter your code from Lab D to print JSON output (with name, email, number1, number2) goes under here
+	printf("{\n");
+    printf("  \"name\": \"Jiayu Yin\",  \n");
+    printf("  \"email\": \"jiayu.yin@student.manchester.ac.uk\",     \n");
+    printf("  \"number1\": %.2f, \n", num);  // Print num formatted to 2 decimal places
+    printf("  \"number2\": %.3e\n", num);  // Print num in scientific notation with 3 digits after the decimal
+    printf("}\n");
+	
+
+	// Do not edit below here
+	return 0;
+}
