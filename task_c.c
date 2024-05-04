ca7f7fbc980667a6c7a1eab933b5ca5f007739cc
Jiayu-Yin <159534253+Jiayu-Yin@users.noreply.github.com>
2024-05-04 16:17:40 +0100

    Add files via upload

---
+++ task_c.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char my_email[] = "jiayu.yin@student.manchester.ac.uk"; // Store the email address
+
+    int n; // Variable to store the user's input for index
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    // Your code below here
+    if (n >= 0 && n < sizeof(my_email) - 1) { // Check if the index is within the valid range
+    
+        switch (my_email[n]) {
+            case 'a': case 'e': case 'i': case 'o': case 'u':  // Vowels
+            case 'A': case 'E': case 'I': case 'O': case 'U':  // Including uppercase vowels
+                printf("Vowel");
+                break;
+            default:
+                printf("Not vowel");
+        }
+    } else {
+        printf("Index out of range!"); // In case the index is out of valid bounds
+    }
+
+    // Do not edit below here
+    return 0;
+}
